9ac1137e579062e439a753d4a0fcae813aebb446
jivallejossaez <j.vallejos.saez@ingeniusolutions.com>
2020-01-28 18:40:02 -0300

    add a car

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/api/users',(req, res) => {
     });
   });
    
-  //update user by ID
+  //update user by ID POR CADA CARACTERISTICA
   app.put('/api/users/:id',(req, res) => {
     let sql = "UPDATE users SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.params.id;
     let query = conn.query(sql, (err, results) => {
@@ -92,6 +92,26 @@ app.get('/api/users',(req, res) => {
   });
 
   //add a new car
+
+  app.post('/api/car',(req, res) => {
+    console.log(req.body);
+  let data = {
+      plate: req.body.plate,
+      alias: req.body.alias, 
+      marca: req.body.marca, 
+      model: req.body.model, 
+      year: req.body.year, 
+      color: req.body.color, 
+      motorId: req.body.motorId, 
+      idUserOwner: req.body.idUserOwner, 
+      idContactEmergency: req.body.idContactEmergency, 
+  };
+  let sql = "INSERT INTO cars SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
 
   //set user as a driver
 
